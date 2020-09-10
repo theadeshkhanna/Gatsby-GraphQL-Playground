82517c2690799ee79771a0e2fb262fb57e295bd6
Adesh Khanna <adeshkhanna271@gmail.com>
2020-09-10 16:53:08 +0530

    [added] our-service page

---
+++ src/pages/service.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "../components/layout"
+
+const ServicePage = () => {
+  return (
+    <Layout>
+      <h1>Our Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam at
+        beatae dicta dolore ea facilis fugiat ipsam iure neque nesciunt nihil nisi
+        optio provident, quas velit voluptatum! Amet deserunt id ipsam, nostrum
+        perferendis quos rem saepe sunt ullam veniam.
+      </p>
+    </Layout>
+  );
+}
+
+export default ServicePage;
