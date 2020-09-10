5186b9f2b7a486c948b3ae1336a4d0ff325e8bf8
Adesh Khanna <adeshkhanna271@gmail.com>
2020-09-10 18:39:00 +0530

    [configured] gatsby-nodeJS

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = ({
+  boundActionCreators,
+  graphql
+}) => {
+  const { createPage } = boundActionCreators
+  const postTemplate = path.resolve(`src/template/blog-post.js`);
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+              author
+              date
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if(res.error) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate
+      })
+    })
+  })
+}
