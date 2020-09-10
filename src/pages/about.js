c94d27c18d418166e06e673542fdc7f2e7c44c7f
Adesh Khanna <adeshkhanna271@gmail.com>
2020-09-10 16:50:19 +0530

    [added] about-us page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam ea
+        eaque eos nulla numquam, suscipit unde veniam. Dolorem ea error esse inventore
+        nam nemo obcaecati quisquam quos voluptatibus voluptatum?
+      </p>
+    </Layout>
+  );
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,14 +7,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to my website</h1>
+    <p>This is a simple site made using gatsbyJS.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
